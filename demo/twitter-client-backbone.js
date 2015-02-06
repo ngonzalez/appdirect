4f8ab0e519ef5d7cdfd28fd6399470b36c987a2b
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2015-02-06 18:29:47 -0500

    Improve use of models

--- demo/twitter-client-backbone.js
+++ demo/twitter-client-backbone.js
@@ -1,24 +1,40 @@
 
+var localStorageKey = "twitter-client"
+
+var TwitterContent = Backbone.Model.extend({
+
+  initialize: function() {
+    this.set('formatted_date', moment(this.get('created_at')).format('MMMM Do YYYY, h:mmA'));
+  }
+
+});
+
 var TwitterUser = Backbone.Model.extend({
 
   urlRoot: function() {
     return "http://0.0.0.0:8080/twitter/" +
       "?" + $.param({
-        screen_name: this.attributes.screen_name,
+        screen_name: this.get('screen_name'),
         count: this.getCount()
       });
   },
 
   initCount: function() {
-    if (!this.getCount()) this.storeCount(10);
+    if (!this.getCount()) {
+      this.storeCount(10);
+      this.set('count', 10);
+    } else {
+      this.set('count', this.getCount());
+    }
   },
 
   storageKey: function() {
-    return "demo-app-" + this.attributes.screen_name;
+    return "demo-app-" + this.get('screen_name');
   },
 
   storeCount: function(count) {
     localStorage.setItem(this.storageKey(), count);
+    this.set('count', count);
   },
 
   getCount: function() {
@@ -35,13 +51,12 @@ var TwitterUserView = Backbone.View.extend({
 
   template: _.template($('#item-template-user').html()),
 
-  initialize: function(item) {
-    this.model = item.model;
+  initialize: function() {
     this.render();
   },
 
   render: function() {
-    return this.template(this.model);
+    return this.template(this.model.toJSON());
   },
 
 });
@@ -50,15 +65,12 @@ var TwitterContentView = Backbone.View.extend({
 
   template: _.template($('#item-template-tweet').html()),
 
-  initialize: function(item) {
-    this.model = item.model;
+  initialize: function() {
     this.render();
   },
 
   render: function() {
-    return this.template($.extend(this.model, {
-      date: moment(this.model.created_at).format('MMMM Do YYYY, h:mmA')
-    }));
+    return this.template(this.model.toJSON());
   }
 
 });
@@ -73,10 +85,11 @@ var TwitterContentsView = Backbone.View.extend({
 
   render: function() {
     _.each(this.collection, function(data) {
-      this.element.append(new TwitterContentView({ model: data }).render());
+      this.element.append(new TwitterContentView({
+        model: new TwitterContent(data)
+      }).render());
     }, this);
-    return this;
-  },
+  }
 
 });
 
@@ -85,23 +98,19 @@ var TwitterUsersView = Backbone.View.extend({
   el: $("#twitter-content"),
 
   initialize: function(collection) {
-    this.collection = new TwitterUserCollection(collection).models;
+    this.collection = new TwitterUserCollection(collection);
     this.render();
   },
 
   render: function() {
-    _.each(this.collection, function(user) {
-      this.displayTweets(user)
-    }, this);
-  },
-
-  displayTweets: function(user) {
-    user.initCount();
-    this.$el.append(new TwitterUserView({ model: $.extend(user.attributes, { count: user.getCount() }) }).render());
-    _.each(this.$el.find("[data-name]"), function(element) {
-      if ($(element).attr("data-name") == user.attributes.screen_name) {
-        this.loadTweetsContent($(element).find(".twitter-content"), user);
-      }
+    _.each(this.collection.models, function(user) {
+      user.initCount();
+      this.$el.append(new TwitterUserView({ model: user }).render());
+      _.each(this.$el.find("[data-name]"), function(element) {
+        if ($(element).attr("data-name") == user.get('screen_name')) {
+          this.loadTweetsContent($(element).find(".twitter-content"), user);
+        }
+      }, this);
     }, this);
   },
 
@@ -125,8 +134,8 @@ var TwitterUsersView = Backbone.View.extend({
     var user_container = $(e.target).closest(".user-container");
     var screen_name = user_container.attr("data-name");
 
-    _.each(this.collection, function(user) {
-      if (user.attributes.screen_name == screen_name) {
+    _.each(this.collection.models, function(user) {
+      if (user.get('screen_name') == screen_name) {
         user.storeCount(count);
         user_container.find(".twitter-content").html("");
         user_container.find(".title-count").html(count)
